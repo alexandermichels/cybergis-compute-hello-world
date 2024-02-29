1880bc88e7d837b645075a788f2936b1f7f942d1
jjhuang4 <112187082+jjhuang4@users.noreply.github.com>
2024-02-29 00:13:52 -0600

    Update testscript.py

--- testscript.py
+++ testscript.py
@@ -27,6 +27,16 @@ gdf['Value'] = [random.randint(1, 100) for _ in range(num_points)]
 
 gdf.head()  # Display the first few rows of the GeoDataFrame
 
+try:
+  gdf = gdf.to_crs(epsg=4326)
+except Exception as e:
+  gdf = gdf.set_crs("EPSG:4326", allow_override=False)
+try:
+  gdf.explore(tiles='OpenStreetMap')
+except Exception as e:
+  print("Failed to display map, encountered error")
+  print(e)
+
 RESULTS_FOLDER = os.getcwd()
 gdf.to_file(os.path.join(RESULTS_FOLDER, "result.shp"))
 print("Saved result to result.shp")

3104a31ecda0f8d219bd512e7326ec4d4c84d95f
jjhuang4 <112187082+jjhuang4@users.noreply.github.com>
2024-04-11 15:38:29 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,4 +2,5 @@
 
 mkdir $result_folder/demo_quick_start_$SLURM_PROCID
 chmod 755 $result_folder/demo_quick_start_$SLURM_PROCID
+wget -P $result_folder https://raw.githubusercontent.com/alexandermichels/alexandermichels.github.io/master/assets/html/maps/CBSA.html
 python main.py
